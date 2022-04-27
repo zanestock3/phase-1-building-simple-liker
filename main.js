35ef4b77728d91fc0ba9bfd8630128026f62ebf2
Zane Stock <zanestock3@gmail.com>
2022-04-27 07:18:23 -0500

    commit tree 6d4e615d3dda94db937d2ceded7b10058346a332

--- main.js
+++ main.js
@@ -3,7 +3,39 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
+document.addEventListener("DOMContentLoaded", () => {
 
+  const hearts = document.querySelectorAll("span.like-glyph")
+  console.log(hearts)
+
+  hearts.forEach(hearts => hearts.addEventListener("click", callB))
+
+  function callB(hearts) {
+    console.log(hearts.target)
+    mimicServerCall()
+    .then( () => {
+      if (hearts.target.innerText === EMPTY_HEART) {
+        hearts.target.classList.add('activated-heart')
+        hearts.target.innerText = FULL_HEART
+      }
+      else {
+        hearts.target.classList.remove('activated-heart')
+          hearts.target.innerText = EMPTY_HEART
+      }
+    })
+    .catch(() => {
+      const errorMess = docuemnt.getElementById("modal")
+      console.log(errorMess)
+      errorMess.classname = "show"
+
+      setTimeout(() => {
+        const errorMess = document.getElementById("modal")
+        console.log(errorMess)
+        errorMess.className = "hidden", 3000
+      })
+    })
+  }
+})
 
 
 
